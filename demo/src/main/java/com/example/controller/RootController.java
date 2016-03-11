277ef0e125118abccd52460c2d840ad388369749
SinghKaushal <s77kaushal@gmail.com>
2016-03-12 01:31:53 +0530

    Update RootController.java

--- demo/src/main/java/com/example/controller/RootController.java
+++ demo/src/main/java/com/example/controller/RootController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +20,9 @@ import static com.example.constant.Constants.*;
 import com.example.model.Brand;
 import com.example.model.WeatherReport;
 import com.example.repository.BrandRepository;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * This is RootController class consists multiple methods , used to handle HTTP request.
@@ -38,22 +43,42 @@ public class RootController {
 	}
 
 	@RequestMapping(value = "/AjaxForWeatherRestApi", method = RequestMethod.POST)
-	Map<String, String> getWhetherData() {
+	Map<String, String> getWhetherData(){
 
 		logger.info("AJAX call is comming for whether report request !!");
 
-		RestTemplate restTemplate = new RestTemplate();
+		/*RestTemplate restTemplate = new RestTemplate();
 		WeatherReport weather = restTemplate.getForObject(WEATHER_URI,
 				WeatherReport.class);
+		*/
+		
+		/* SINCE  Provided API ("http://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=44db6a862fba0b067b1930da0d769e98")
+		 * is not working we need , I am just providing the json Data in form of a .json file and   
+		 * 
+		 *  */
+		
+		ObjectMapper mapper = new ObjectMapper();
+		WeatherReport weather;
+		try {
+			weather = mapper.readValue(new File("jsonFile.json"), WeatherReport.class);
+			Map<String, String> mapVal = new HashMap<String, String>();
+			mapVal.put(NAME, weather.getName());
+			mapVal.put(LON, weather.getCoord().getLon().toString());
+			mapVal.put(LAT, weather.getCoord().getLat().toString());
+			mapVal.put(DESCRIPTION, weather.getWeather().get(0).getDescription());
 
-		Map<String, String> mapVal = new HashMap<String, String>();
-		mapVal.put(NAME, weather.getName());
-		mapVal.put(LON, weather.getCoord().getLon().toString());
-		mapVal.put(LAT, weather.getCoord().getLat().toString());
-		mapVal.put(DESCRIPTION, weather.getWeather().get(0).getDescription());
+			return mapVal;
 
-		return mapVal;
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+
+		return null;
 	}
 
 	@RequestMapping(value = "/AjaxForDBData", method = RequestMethod.GET)
